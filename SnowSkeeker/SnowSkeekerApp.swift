8581932c0ec85f1dab707f4f2aa7411a031d1c32
Carson <carsongro@gmail.com>
2022-07-14 07:45:41 -0600

    Initial Commit

---
+++ SnowSkeeker/SnowSkeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSkeekerApp.swift
+//  SnowSkeeker
+//
+//  Created by Carson Gross on 7/14/22.
+//
+
+import SwiftUI
+
+@main
+struct SnowSkeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
